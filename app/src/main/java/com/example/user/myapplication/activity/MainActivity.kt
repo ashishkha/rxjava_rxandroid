ee400f2e1cfbb296e08d52b4fecaaa29237e1bd4
ashishkha <ashishkha1000@gmail.com>
2019-01-24 19:46:55 +0530

    basic code for rxjava implemented

--- app/src/main/java/com/example/user/myapplication/activity/MainActivity.kt
+++ app/src/main/java/com/example/user/myapplication/activity/MainActivity.kt
@@ -2,7 +2,6 @@ package com.example.user.myapplication.activity
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -11,35 +10,36 @@ import io.reactivex.schedulers.Schedulers
 
 class MainActivity : AppCompatActivity() {
 
+
+    private var observable: Observable<String>? = null
+    private var observer: Observer<String>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val observable = Observable.just("hello","world","world1","ashish")
+        observable = Observable.just("a", "b", "c", "d", "e")
 
-        val observer = getObserver()
+        observer = getDataFromObserver()
 
         observable
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(observer)
+                ?.subscribeOn(Schedulers.io())
+                ?.observeOn(AndroidSchedulers.mainThread())
+                ?.subscribe(observer!!)
+
     }
 
-    private fun getObserver(): Observer<String> {
+    private fun getDataFromObserver(): Observer<String> {
         return object : Observer<String> {
             override fun onComplete() {
-                Log.d(TAG,"check_ak complete")
             }
 
             override fun onSubscribe(d: Disposable) {
-                Log.d(TAG,"check_ak subscribe")
             }
 
             override fun onNext(t: String) {
-                Log.d(TAG,"check_ak next:"+t)
             }
 
             override fun onError(e: Throwable) {
-                Log.d(TAG,"check_ak error")
             }
 
         }
